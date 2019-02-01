8701a9a7ebdc508ebaa8ec1c79288492069eb703
wujia <ms-wujia@hotmail.com>
2019-02-02 00:03:55 +0800

    添加规约

---
+++ GAM.Domain/Entities/Specification/ISpecification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace GAM.Domain.Entities.Specification
+{
+    /// <summary>
+    /// 规约接口
+    /// </summary>
+    public interface ISpecification<T> where T: class, Aggregates.IAggregateRoot
+    {
+        bool IsSatisfiedBy(T candidate);
+
+        Expression<Func<object, bool>> Expression { get; }
+
+        //ISpecification<T> And(ISpecification<T> spec);
+
+        //ISpecification<T> Or(ISpecification<T> spec);
+
+        //ISpecification<T> Not(ISpecification<T> spec);
+    }
+}
